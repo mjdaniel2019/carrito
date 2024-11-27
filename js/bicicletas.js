8c48e8f7aa000a58d30a7e9291ee4b2ef4870233
mjdaniel2019 <mjdaniel@escuelasproa.edu.ar>
2024-11-27 11:35:08 -0300

    Add files via upload

---
+++ js/bicicletas.js
@@ -0,0 +1,33 @@
+/* Esta lista podría ser reemplazada por la respuesta de un backend */
+const bicicletas = [
+  {
+    id:1,
+    nombre: "Dog chow",
+    precio: 40000,
+  },
+  {
+    id:2,
+    nombre: "Puppy chow",
+    precio: 40000,
+  },
+  {
+    id:3,
+  nombre: "kibbles 'nBits",
+    precio: 30000,
+  },
+  {
+    id:4,
+    nombre: "Pedigree",
+    precio: 40000,
+  },
+  {
+    id:5,
+    nombre: "Red Mills",
+    precio: 50000,
+  },
+  {
+    id:6,
+    nombre: "AUTARKY",
+    precio: 100000,
+  }
+]
